0bf1a45af635d17164a120c9ddd977f670684c27
filipw <wojcieszyn@gmail.com>
2016-08-17 20:18:23 +0200

    first sample version

---
+++ src/LightweightSiteSample/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using WebApiContrib.Core.WebPages;
+
+namespace LightweightSiteSample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebPages(new WebPagesOptions { RootViewName = "Index" });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseWebPages();
+        }
+    }
+}
